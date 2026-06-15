c9748a5fa9cdf222bc2a19dc2e411099575a5295
diogoaugustonavais <diogonavais10@gmail.com>
2026-06-15 16:13:28 -0300

    Create 04.js

---
+++ 04.js
@@ -0,0 +1,4 @@
+let valor_total = Number(prompt("Digite o valor total da conta"));
+let amigos = Number(prompt("Digite a quantidade de amigos"));
+const pagar = valor_total/amigos;
+console.log("cada amigo terá que pagar R$"+pagar+".");
