f4efea1980f5a937301df8db26671d4f8e0df6ac
diogoaugustonavais <diogonavais10@gmail.com>
2026-06-15 16:16:30 -0300

    Create 05.js

---
+++ 05.js
@@ -0,0 +1,5 @@
+let nota1 = Number(prompt("Digite a primeira nota do aluno"));
+let nota2 = Number(prompt("Digite a primeira nota do aluno"));
+let nota3 = Number(prompt("Digite a primeira nota do aluno"));
+const media = nota1+nota2+nota3/3
+console.log("A média das notas é "+media+".");
