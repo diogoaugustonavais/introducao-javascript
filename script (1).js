94c1fc857aab813bc77658fd2a3bbfe66b462347
diogoaugustonavais <diogonavais10@gmail.com>
2026-06-15 16:00:25 -0300

    Add files via upload

---
+++ script (1).js
@@ -0,0 +1,5 @@
+let ano = Number(prompt("Digite o ano em que você nasceu"));
+let ano_atual = Number(prompt("Digite o ano atual"));
+const aprox = ano_atual - ano;
+console.log("Você tem aproximadamente "+ aprox+" anos");
+alert("Você tem aproximadamente b" + aprox+" anos");
