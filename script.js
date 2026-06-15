a46e7f6f0910b01ee8931c9c4072a995138a2f8d
diogoaugustonavais <diogonavais10@gmail.com>
2026-06-15 15:49:23 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+let numero1 = Number(prompt("Digite numero para somar"));
+let numero2 = Number(prompt("Digite numero para somar"));
+const resultado = numero1 + numero2;
+console.log("O resultado da soma é: " + resultado);
