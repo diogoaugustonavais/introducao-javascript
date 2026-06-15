25fefcbdfd1552771267e2ebd7d0f8fe87f6d754
diogoaugustonavais <diogonavais10@gmail.com>
2026-06-15 16:09:30 -0300

    Add room area calculation in 03.js

---
+++ 03.js
@@ -0,0 +1,4 @@
+let largura = Number(prompt("Digite a largura da sala (em metros)"));
+let comprimento = Number(prompt("Digite o comprimento da sala (em metros)"));
+const metros = largura * comprimento;
+console.log("A sala tem "+metros+" metros quadrados.");
