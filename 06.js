ea15c63c868260e52ff251026ec4fb5c9d4b0753
diogoaugustonavais <diogonavais10@gmail.com>
2026-06-15 16:21:17 -0300

    Create 06.js

---
+++ 06.js
@@ -0,0 +1,3 @@
+let peca = Number(prompt("Digite o valor do par de sapatos"));
+const desconto = peca * 0.85
+console.log("O valor do par de sapatos é R$"+peca+", mas com 15% de desconto é R$"+desconto+".");
